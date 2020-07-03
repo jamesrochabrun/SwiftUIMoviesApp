ffb5953a7aee6f846e6198f7ce8938ecbfc719b5
james rochabrun <jamesrochabrun@gmail.com>
2020-07-02 17:35:55 -0700

    Initial Commit

---
+++ SwiftUISearchExample/SwiftUISearchExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISearchExampleApp.swift
+//  SwiftUISearchExample
+//
+//  Created by James Rochabrun on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISearchExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
